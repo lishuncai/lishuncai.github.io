6a01eabc36e19e4810c4bc1b6544d527bf9ddb09
lishuncai <1832481826@qq.com>
2018-01-25 10:05:30 +0800

    new build in 1-25
    a project for musicApp

---
+++ 网易云/javascript/index.js
@@ -0,0 +1,125 @@
+//轮播图；
+var imgs=[];
+for(var i=0;i<6;i++){
+	var url='imgs/轮播图/'+i+'.jpg';
+	imgs.push(url);
+}
+//重复三张图片
+imgs.push(imgs[0],imgs[1],imgs[2]);
+
+//栏目
+var Groups=[
+{head:'推荐歌单', cla:'Recom',name:'Recom'},
+{head:'独家放送', cla:'special',name:'special'},
+{head:'最新音乐', cla:'new',name:'new'},
+{head:'推荐MV', cla:'MV',name:'MV'},
+{head:'主播电台', cla:'radio',name:'radio'}
+];
+var Groups_content=[listDetails,listDetails];
+//推荐歌单
+var dec=[
+	'幻想净琉璃',
+	'refrain',
+	'古明地灵殿',
+	'秘封活动记录',
+	'花之舞',
+	'天空花都',
+	'东方交响乐',
+	'彩月',
+	'古树旋律',
+	'东方风神录'
+]
+var bgImgs=bgimgs();
+	function bgimgs(){
+		var imgs_url=[];
+		for(var i=0;i<10;i++){
+			imgs_url.push('url(./imgs/推荐歌单/'+i+'.jpg)');
+		}
+		return imgs_url
+	}
+var listDetails={
+	played:function(){
+		return parseInt(Math.random()*1000)+'万'
+	},
+	dec:dec,
+	bgImage:bgImgs,
+	name:'Recom'
+}
+
+//最新音乐
+var newestSong={
+	pics:function(){
+		var r=Math.floor(Math.random()*256);
+		var g=Math.floor(Math.random()*256);
+		var b=Math.floor(Math.random()*256);
+		return 'rgb('+r+','+g+','+b+')';
+		
+	},
+	songname:[],
+	author:[]
+};
+build();
+function build(){
+	for(var i=97;i<107;i++){
+		newestSong.songname.push('歌曲'+String.fromCharCode(i));
+		newestSong.author.push('作者'+String.fromCharCode(i-32));
+	}
+}
+
+Vue.component(
+	'Recom',{
+		template:
+		`<div>
+			<p class='h'>
+				<span>推荐歌单</span>
+				<span class='more'>更多 &gt;</span>
+			</p>
+			<div class='lists'>
+				<div v-for='(dec,index) in list' class='list'>
+					<div class='dec'>
+						<span class='num'>{{listsong.played()}}</span>
+					</div>
+					<span>{{dec}}</span>
+				</div>
+			</div>
+		</div>`,
+		props:['listsong','list'],
+		data:function(){
+			return {
+				num: listDetails.played
+			}
+		},
+		mounted:function(){
+			var dec=document.querySelectorAll('.dec');
+			for(var i=0;i<dec.length;i++){
+				dec[i].style.backgroundImage=listDetails.bgImage[i];
+				dec[i].style.backgroundRepeat='norepeat';
+				dec[i].style.backgroundSize='100% 100%'
+			}
+		}
+	}
+)
+Vue.component('songs',{
+		template:'#newsong',
+		props:{
+			song:Object
+		},
+		data:function(){
+			return {
+				items:newestSong,
+				colorBlock:function(){
+					return "backgroundColor:"+newestSong.pics()
+				}
+			}
+		}
+	})
+
+var app=new Vue({
+	el:"#musicApp",
+	data:{
+		items:imgs,
+		Groups:Groups,
+		listDetails:listDetails,
+		newsong:newestSong
+	}
+});
---
+++ 网易云/javascript/图片轮播.js
@@ -0,0 +1,176 @@
+//原生js图片轮播
+var n=0;
+var timer=null;
+var speed=1500;
+
+var contains=document.querySelector('.center-contains'),
+ 	controls=contains.querySelector('.controls'),
+	imgsPlay=contains.querySelector('.imgs-play'),
+	but_left=contains.querySelector('.left'),
+	but_right=contains.querySelector('.right'),
+	marks=contains.querySelectorAll('.mark');
+
+var items=document.querySelector('#items');
+	itemArr=items.querySelectorAll('.item'),
+	imgs=items.querySelectorAll('img'),
+	item=itemArr[0];
+var itemWidth;
+
+	
+Object.prototype.addEvent=function(evenType,callback,nostop){
+	this.addEventListener(evenType,function(e){
+		var e=e||window.event;
+		//如果nostop为true，可根据情况手动阻止冒泡或默认行为;
+		if(nostop){
+			callback();
+			// callback.call(this);//this为点击对象
+		}else{
+			e.stopPropagation();
+			e.preventDefault();
+			callback.call(this);
+		}
+	},nostop);
+}
+
+//事件的区别，普通的方式不能同时添加多个事件函数，会取最后的事件函数；
+//监听机制可以绑定多个事件；
+//鼠标移入画廊;
+function imgsPlay_over(){
+	clearTimeout(timer);
+	but_left.style.opacity=1;
+	but_right.style.opacity=1;
+}
+//移出画廊
+function imgsplay_out(){
+	but_left.style.opacity=0;
+	but_right.style.opacity=0;
+	timer=setTimeout(play,speed);
+	this.removeEventListener('mouseover',imgsPlay_over);
+	this.removeEventListener('mouseout',imgsplay_out);
+}
+//监听画廊事件；
+items.addEvent('mouseover',imgsPlay_over,true);
+items.addEvent('mouseout',imgsplay_out,true);
+
+
+//鼠标点击按钮
+function butDown(){
+	this.style.transform='scale(0.8)';
+}
+function butUp(){
+	play(this.derection);
+}
+//鼠标移入按钮
+function butOver(){
+	this.style.opacity=1;
+	this.addEventListener('click',butUp,false);
+	this.addEventListener('mouseout',butOut,false);
+}
+//移出按钮
+function butOut(){
+	this.removeEventListener('mousedown',butDown);
+	this.removeEventListener('mouseup',butUp);
+	this.removeEventListener('mouseout',butOut);
+
+}
+//监听按钮事件
+but_left.derection='left';
+but_right.derection='right';
+but_left.addEventListener('mouseover',butOver,false);
+but_right.addEventListener('mouseover',butOver,false);
+
+imgs.forEach(function(key){
+	key.big=function(){
+		this.style.transform='scale(1.2)'+'translateY(-7%)';
+	}
+	key.small=function(){
+		this.style.transform='scale(1)'+'translateY(0)';
+	}
+	key.addEvent('mouseover',function(){
+		var restyle=this.style.filter;
+		this.style.filter+='contrast(0.6)';
+		this.addEvent('mouseout',function(){
+			this.style.filter=restyle;
+		})
+	},false)
+})
+
+play();
+function play(derection){
+	clearTimeout(timer);
+	if(!items.hasAttribute('move')){
+		items.setAttribute('move',"");
+	}
+	//contains.offsetWidth的值是随窗口变化的
+	itemWidth=contains.offsetWidth/3;
+	if(derection==='left'){
+		left();
+	}else{
+		right();
+	}
+	
+	timer=setTimeout(play,speed);	
+}
+function left(){
+	if(n===0){
+		n=6;
+		imgs[7].removeAttribute('scale');
+		imgs[7].big();
+		imgs[7].style.filter='brightness(160%)';
+		imgs[7].parentNode.style.zIndex=99;
+		items.removeAttribute('move');
+		items.style.marginLeft= -6* itemWidth+'px';
+		imgs[1].setAttribute('scale','small');
+		imgs[1].small();
+		imgs[1].removeAttribute('scale');
+	}else{
+		n-=1;
+		items.style.marginLeft=-n*itemWidth+'px';
+		imgs[n+2].setAttribute('scale','small');
+		imgs[n+2].small();
+		imgs[n+2].style.filter='brightness(100%)';
+		imgs[n+1].setAttribute('scale','big');
+		imgs[n+1].big();
+		imgs[n+1].style.filter='brightness(160%)';
+
+	}
+	// items.style.marginLeft=items.offsetLeft+itemWidth+'px';
+	// n-=1;
+}
+function right(){
+	if(n===6){
+		n=0;
+		imgs[1].removeAttribute('scale');
+		imgs[1].big();
+		imgs[1].style.filter='brightness(160%)';
+		//关键句，解决imgs[2]覆盖imgs[1]的问题；
+		//元素声明transform时，层级变得很高，兄弟元素同等级别情况下，后来元素居上。
+		//所以这里把imgs[1]的老爸请出来了。此处感谢张鑫旭！
+		imgs[1].parentNode.style.zIndex=99;
+		items.removeAttribute('move');
+		items.style.marginLeft= 0+'px';
+		imgs[7].setAttribute('scale','small');
+		imgs[7].small();
+		imgs[7].removeAttribute('scale');
+	}else{
+		//这里必须用else,否则达不到无缝的效果
+		n+=1;
+		items.style.marginLeft=-n*itemWidth+'px';
+		imgs[n].setAttribute('scale','small');
+		imgs[n].small();
+		imgs[n].style.filter='brightness(100%)';
+		imgs[n+1].setAttribute('scale','big');
+		imgs[n+1].style.filter='brightness(160%)';
+		imgs[n+1].big();
+	}
+	marks.forEach(function(key){
+		key.style.backgroundColor='';
+	});
+	marks[n].style.backgroundColor='#ccc';
+}
+
+window.onresize=function(){
+	clearTimeout(timer);	
+	items.removeAttribute('move');
+	timer=setTimeout(play,500);
+}
