3215232326ed03cff21904c2bcd51248aafff023
lishuncai <1832481826@qq.com>
2018-03-08 13:35:25 +0800

    3-8

--- javascript/图片轮播.js
+++ javascript/图片轮播.js
@@ -1,160 +1,156 @@
 /*图片轮播 imgsPlay*/
 /*重点在于临界处理,先监测值再增值；*/
-var imgsPlay = document.getElementById('imgsPlay');
-var leftBut = imgsPlay.querySelector('.leftBut'),
-  rightBut = imgsPlay.querySelector('.rightBut');
-var items = imgsPlay.querySelector('#items');
-items.move = true;
-var itemArr = items.querySelectorAll('.item');
-itemArr[1].setAttribute('scale', 'big'); //受其他进程影响，这里会报错，事实上这里不会有错的
-var marks = imgsPlay.querySelectorAll('.marks >span');
-var timer; //定时器；
-var n = 0; //系数；
-var L; //图片数量；
-var speed = 2000; //定时器间隔；
-var timerMove; // 延迟移动的定时器；
-var timersetAttr; //延迟赋予属性的定时器；
-var timerClick; //点击定时器，用来限制点击之间
+var imgsPlay=document.getElementById('imgsPlay');
+var leftBut=imgsPlay.querySelector('.leftBut'),
+    rightBut=imgsPlay.querySelector('.rightBut');
+var items=imgsPlay.querySelector('#items');
+    items.move=true;
+var itemArr=items.querySelectorAll('.item');
+    itemArr[1].setAttribute('scale','big');//受其他进程影响，这里会报错，事实上这里不会有错的
+var marks=imgsPlay.querySelectorAll('.marks >span');
+var timer;  //定时器；
+var n=0;  //系数；
+var L;  //图片数量；
+var speed=2000; //定时器间隔；
+var timerMove;    // 延迟移动的定时器；
+var timersetAttr;   //延迟赋予属性的定时器；
+var timerClick;   //点击定时器，用来限制点击之间
 //将定时器和运动函数分离，方便扩张
-L = itemArr.length - 3;
-items.setAttribute('move', '');
+L=itemArr.length-3;
+items.setAttribute('move','');
 setTimer();
-
-function setTimer() {
+function setTimer(){
   clearTimeout(timer);
-  timer = setTimeout(setTimer, speed);
+  timer=setTimeout(setTimer,speed);
   move();
 }
-
-function move(dir) {
-  dir = dir || 'right';
-  if (dir === 'right') {
-    if (n > L - 1) {
+function move(dir){
+  dir=dir||'right';
+  if(dir==='right'){
+    if(n>L-1){
       //移除元素过度属性
-      n = 0;
-      items.move = false;
+      n=0;
+      items.move=false;
       items.removeAttribute('move');
-      itemArr[L + 1].removeAttribute('scale');
+      itemArr[L+1].removeAttribute('scale');
       itemArr[1].removeAttribute('scale');
-      itemArr[1].setAttribute('transform', '');
-      items.style.marginLeft = -(n / 3) * 100 + '%';
+      itemArr[1].setAttribute('transform','');
+      items.style.marginLeft= -(n/3)*100+'%';
     }
     n++;
   }
-  if (dir === 'left') {
-    if (n < 1) {
-      n = L;
+  if(dir==='left'){
+    if(n<1){
+      n=L;
       items.removeAttribute('move');
-      items.move = false;
+      items.move=false;
       itemArr[1].removeAttribute('scale');
-      itemArr[L + 1].removeAttribute('scale');
-      itemArr[L + 1].setAttribute('transform', '');
-      items.style.marginLeft = -(n / 3) * 100 + '%';
+      itemArr[L+1].removeAttribute('scale');
+      itemArr[L+1].setAttribute('transform','');
+      items.style.marginLeft= -(n/3)*100+'%';
     }
     n--;
   }
   //恢复上面移除的属性，设置时间间隔不小于200ms,
   //避免恢复属性后因时间间隔过短影响上面的属性取消操作；
-  if (items.move === false) {
+  if(items.move===false){
     //items.move=false时表示已经替换位置了
-    items.move = true;
+    items.move=true;
     clearTimeout(timersetAttr);
-    timersetAttr = setTimeout(function() {
-      items.setAttribute('move', '');
-      if (dir === 'right') {
+    timersetAttr=setTimeout(function(){
+      items.setAttribute('move','');
+      if(dir==='right'){
         itemArr[1].removeAttribute('transform');
-        itemArr[1].setAttribute('scale', 'small');
-        itemArr[L + 1].setAttribute('scale', 'small');
-      } else {
-        itemArr[L + 1].removeAttribute('transform');
-        itemArr[L + 1].setAttribute('scale', 'small');
-        itemArr[1].setAttribute('scale', 'small');
+        itemArr[1].setAttribute('scale','small');
+        itemArr[L+1].setAttribute('scale','small');
+      }else{
+        itemArr[L+1].removeAttribute('transform');
+        itemArr[L+1].setAttribute('scale','small');
+        itemArr[1].setAttribute('scale','small');
       }
-    }, 200)
+    },200)
   }
-
-  function scaleRight() {
-    itemArr[n + 1].setAttribute('scale', 'big');
-    itemArr[n].setAttribute('scale', 'small');
+  function scaleRight(){
+    itemArr[n+1].setAttribute('scale','big');
+    itemArr[n].setAttribute('scale','small');
   }
-
-  function scaleLeft() {
-    itemArr[n + 1].setAttribute('scale', 'big');
-    itemArr[n + 2].setAttribute('scale', 'small');
+  function scaleLeft(){
+    itemArr[n+1].setAttribute('scale','big');
+    itemArr[n+2].setAttribute('scale','small');
   }
   clearTimeout(timerMove);
-  timerMove = setTimeout(function() {
-    if (dir === 'left') {
+  timerMove=setTimeout(function(){
+    if(dir==='left'){
       scaleLeft();
       markJump(n)
     }
-    if (dir === 'right') {
+    if(dir==='right'){
       scaleRight();
-      markJump(n - 1);
+      markJump(n-1);
     }
-    items.style.marginLeft = -(n / 3) * 100 + '%';
-    /* 除以3是因为margin-left比例是根据父标签imgsPlay的width值决定的,
-    以达到自适应效果，窗口缩放时移动距离有偏差这两个bug*/
-  }, 200)
+  items.style.marginLeft= -(n/3)*100+'%';
+  /* 除以3是因为margin-left比例是根据父标签imgsPlay的width值决定的,
+  以达到自适应效果，窗口缩放时移动距离有偏差这两个bug*/
+  },200)
 }
 //点击下面的小圆圈
 //好吧，因为某种复杂的原因，第一个圆圈对应itemArr[2]
-function markJump(index) {
-  for (var j = 0; j < marks.length; j++) {
-    marks[j].style.background = 'none';
+function markJump(index){
+  for(var j=0;j<marks.length;j++){
+    marks[j].style.background='none';
   }
-  marks[index].style.background = 'white';
+  marks[index].style.background='white';
 }
-for (var i = 0; i < marks.length; i++) {
-  (function(index) {
-    marks[index].onclick = function() {
-      itemArr[n + 1].setAttribute('scale', 'small');
-      n = index + 1;
-      items.style.marginLeft = -((index + 1) / 3) * 100 + '%';
-      itemArr[index + 2].setAttribute('scale', 'big');
+for(var i=0;i<marks.length;i++){
+  (function(index){
+    marks[index].onclick=function(){
+      itemArr[n+1].setAttribute('scale','small');
+      n=index+1;
+      items.style.marginLeft= -((index+1)/3)*100+'%';
+      itemArr[index+2].setAttribute('scale','big');
       markJump(index);
     }
   })(i);
 }
 
-imgsPlay.onmouseover = function() {
+imgsPlay.onmouseover=function(){
   clearTimeout(timer);
-  leftBut.style.visibility = 'visible';
-  rightBut.style.visibility = 'visible'
+  leftBut.style.visibility='visible';
+  rightBut.style.visibility='visible'
 }
-imgsPlay.onmouseout = function() {
-  timer = setTimeout(setTimer, speed);
-  leftBut.style.visibility = 'hidden';
-  rightBut.style.visibility = 'hidden'
+imgsPlay.onmouseout=function(){
+  timer=setTimeout(setTimer,speed);
+  leftBut.style.visibility='hidden';
+  rightBut.style.visibility='hidden'
 }
 
-function cut(dir) {
+function cut(dir){
   clearTimeout(timerClick);
-  timerClick = setTimeout(function() {
+  timerClick=setTimeout(function(){
     move(dir);
-  }, 200)
+  },200)
 }
-leftBut.onclick = function() {
-  cut('left') //不能小于200ms;因为上面移动时设置了200ms的间距
+leftBut.onclick=function(){
+  cut('left')  //不能小于200ms;因为上面移动时设置了200ms的间距
 }
-rightBut.onclick = function() {
+rightBut.onclick=function(){
   cut('right')
 }
 //鼠标滚轮事件
-function wheel(e, value) {
-  e = e || window.event;
-  if (e.wheelDelta) {
-    value = e.wheelDelta / 120
-  } else if (e.detail) {
-    value = -e.detail / 3
+function wheel(e,value){
+  e=e||window.event;
+  if(e.wheelDelta){
+    value=e.wheelDelta/120
+  }else if(e.detail){
+    value=-e.detail/3
   }
-  if (value < 0) {
+  if(value<0){
     cut('right')
-  } else {
+  }else{
     cut('left')
   }
   e.preventDefault();
   e.stopPropagation();
 }
-items.onmousewheel = wheel;
-items.addEventListener('DOMMouseScroll', wheel, false);
+items.onmousewheel=wheel;
+items.addEventListener('DOMMouseScroll',wheel,false);
--- javascript/播放控件.js
+++ javascript/播放控件.js
@@ -1,113 +1,113 @@
-var audioInput = document.getElementById('audio');
-var ac; //音频环境
-var gainNode; //音量节点
-var analyser; //分析节点
-var ArrayBuffer; //
-var bufferSource; //音源缓冲
-var source; //音源缓冲
+var audioInput=document.getElementById('audio');
+var ac;//音频环境
+var gainNode;//音量节点
+var analyser;//分析节点
+var ArrayBuffer;//
+var bufferSource;//音源缓冲
+var source;//音源缓冲
 var size;
-var Hots = []; //柱子上的帽子
+var Hots=[];//柱子上的帽子
 var times;
 var animationFrame;
-var circlesAnimation; //进度条动画
-var range = document.querySelector('#range');
-var shownum = document.querySelector('#num');
-var fileLoadResolve = false;
-var count = 0;
+var circlesAnimation;//进度条动画
+var range=document.querySelector('#range');
+var shownum=document.querySelector('#num');
+var fileLoadResolve=false;
+var count=0;
 
-audioInput.onchange = function() {
-  if (!audio.files[0]) {
+audioInput.onchange=function(){
+  if(!audio.files[0]){
     return
-  } else if (audio.files.length !== 0) {
+  }else if(audio.files.length!==0){
     source && source.stop();
-    var file = audio.files[0];
-    var reader = new FileReader();
+    var file=audio.files[0];
+    var reader=new FileReader();
     reader.readAsArrayBuffer(file);
     // if(reader.readyState===2){}
-    reader.onload = function() {
-      fileLoadResolve = true;
-      build(reader, 0);
-      app.play = true; //来自vue对象的数据
+    reader.onload=function(){
+      fileLoadResolve=true;
+      build(reader,0);
+      app.play=true;//来自vue对象的数据
     };
   }
 }
 
-function build(reader, when) {
-  var n = ++count;
-  ac = new(window.AudioContext || window.webkitAudioContext || window.mozAudioContext)();
-  // gainNode=ac.createGain();
-  // gainNode.connect(ac.destination);
-  analyser = ac.createAnalyser();
-  analyser.fftSize = 32;
-  // analyser.connect(gainNode);
-  analyser.connect(ac.destination);
-  ArrayBuffer = reader.result;
+function build(reader,when){
+      var n= ++count;
+      ac=new (window.AudioContext||window.webkitAudioContext||window.mozAudioContext)();
+      // gainNode=ac.createGain();
+      // gainNode.connect(ac.destination);
+      analyser=ac.createAnalyser();
+      analyser.fftSize=32;
+      // analyser.connect(gainNode);
+      analyser.connect(ac.destination);
+      ArrayBuffer=reader.result;
 
-  ac.decodeAudioData(ArrayBuffer, function(buffer) {
-    if (n != count) return;
-    bufferSource = ac.createBufferSource();
-    bufferSource.buffer = buffer;
-    times = bufferSource.buffer.duration;
-    size = analyser.frequencyBinCount - 4;
-    bufferSource.connect(analyser);
-    bufferSource.start(0, when, bufferSource.buffer.duration);
-    source = bufferSource;
+      ac.decodeAudioData(ArrayBuffer,function(buffer){
+        if(n != count)return;
+        bufferSource=ac.createBufferSource();
+        bufferSource.buffer=buffer;
+        times=bufferSource.buffer.duration;
+        size=analyser.frequencyBinCount-4;
+        bufferSource.connect(analyser);
+        bufferSource.start(0,when,bufferSource.buffer.duration);
+        source=bufferSource;
 
-    width = canvas.width / size;
-    for (var i = 0; i < size; i++) {
-      Hots.push({
-        y: canvas.height / 2
+        width=canvas.width/size;
+        for(var i=0;i<size;i++){
+          Hots.push({
+            y:canvas.height/2
+          })
+        }
+        draw();
+        // volume(range.value/100);
+        playDuration(when);
+        sliding();
+      },function(err){
+        alert('请选择音频文件');
+        console.error(err);
       })
     }
-    draw();
-    // volume(range.value/100);
-    playDuration(when);
-    sliding();
-  }, function(err) {
-    alert('请选择音频文件');
-    console.error(err);
-  })
-}
 
-var circles = document.querySelectorAll('.circles')[0];
-var duration = document.querySelectorAll('.duration')[0];
-var canvas = document.getElementById('canvas');
-var canvasCtx = canvas.getContext('2d');
+var circles=document.querySelectorAll('.circles')[0];
+var duration=document.querySelectorAll('.duration')[0];
+var canvas=document.getElementById('canvas');
+var canvasCtx=canvas.getContext('2d');
 var width;
-var controlsTime = document.querySelector('.controls-time');
-var durationRed = document.querySelector('.duration-red');
+var controlsTime=document.querySelector('.controls-time');
+var durationRed=document.querySelector('.duration-red');
 
-function draw() {
+function draw(){
   window.cancelAnimationFrame(animationFrame);
-  var arr = new Uint8Array(size);
+  var arr=new Uint8Array(size);
   analyser.getByteFrequencyData(arr);
 
-  canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < size; i++) {
-    var height = arr[i];
-    canvasCtx.fillStyle = 'rgba(255,255,255,1)';
-    canvasCtx.lineWidth = 0.1;
-    canvasCtx.strokeStyle = 'rgba(255,255,255,0.1)';
-    canvasCtx.fillRect((i) * width, canvas.height / 2, width / 2, -arr[i] * 0.2);
-    canvasCtx.fillRect((i) * width, Hots[i].y, width / 2, -1);
+  canvasCtx.clearRect(0,0,canvas.width,canvas.height);
+  for(var i=0;i<size;i++){
+    var height=arr[i];
+    canvasCtx.fillStyle='rgba(255,255,255,1)';
+    canvasCtx.lineWidth=0.1;
+    canvasCtx.strokeStyle='rgba(255,255,255,0.1)';
+    canvasCtx.fillRect((i)*width,canvas.height/2,width/2,-arr[i]*0.2);
+    canvasCtx.fillRect((i)*width,Hots[i].y,width/2,-1);
     //左上
-    canvasCtx.fillRect((i) * width, Hots[i].y, width / 2, -1);
-    canvasCtx.fillStyle = 'rgba(0,0,0,0.2)';
-    canvasCtx.fillRect((i) * width, canvas.height / 2, width / 2, arr[i] * 0.1);
+    canvasCtx.fillRect((i)*width,Hots[i].y,width/2,-1);
+    canvasCtx.fillStyle='rgba(0,0,0,0.2)';
+    canvasCtx.fillRect((i)*width,canvas.height/2,width/2,arr[i]*0.1);
 
-    Hots[i].y += 0.18;
-    if (Hots[i].y < 0) {
-      Hots[i].y = 0;
+    Hots[i].y+=0.18;
+    if(Hots[i].y<0){
+      Hots[i].y=0;
     }
-    if (Hots[i].y > canvas.height / 2) {
-      Hots[i].y = canvas.height / 2;
+    if(Hots[i].y>canvas.height/2){
+      Hots[i].y=canvas.height/2;
     }
-    if (Hots[i].y > canvas.height / 2 - arr[i] * 0.2) {
-      Hots[i].y -= 4;
+    if(Hots[i].y>canvas.height/2-arr[i]*0.2){
+      Hots[i].y-=4;
     }
 
   }
-  animationFrame = window.requestAnimationFrame(draw);
+  animationFrame=window.requestAnimationFrame(draw);
 }
 
 
@@ -123,20 +123,20 @@ function draw() {
 
 
 //进度条播放
-function playDuration(when) {
-  if (ac.currentTime && when + ac.currentTime >= times) {
-    circles.style.left = 0 + 'px';
-    durationRed.style.width = 0;
+function playDuration(when){
+  if(ac.currentTime && when+ac.currentTime>=times){
+    circles.style.left=0+'px';
+    durationRed.style.width=0;
     window.cancelAnimationFrame(animationFrame);
-    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    canvasCtx.clearRect(0,0,canvas.width,canvas.height);
     console.log('播放完成');
-    app.play = false;
+    app.play=false;
     ac.close();
-  } else {
-    var time = Math.floor((when + ac.currentTime) / times * duration.offsetWidth);
-    circles.style.left = time + 'px';
-    durationRed.style.width = time + 'px';
-    circlesAnimation = window.requestAnimationFrame(function() {
+  }else{
+    var time=Math.floor((when+ac.currentTime)/times *duration.offsetWidth);
+    circles.style.left=time+'px';
+    durationRed.style.width=time+'px';
+    circlesAnimation=window.requestAnimationFrame(function(){
       playDuration(when);
     });
     /*这里有个问题,如果用
@@ -147,83 +147,81 @@ function playDuration(when) {
 }
 
 //滑动进度条,跳播
-function acRebuild(when) {
-  if (ac.state !== 'closed') {
+function acRebuild(when){
+  if(ac.state!=='closed'){
     ac.close()
-  } else {
+  }else{
     return
   }
-  var file = audio.files[0];
-  var reader = new FileReader();
+  var file=audio.files[0];
+  var reader=new FileReader();
   reader.readAsArrayBuffer(file);
-  reader.onload = function() {
-    build(reader, when)
+  reader.onload=function(){
+    build(reader,when)
   }
 
 }
 
 
-function sliding() {
+function sliding(){
   var L;
   var touch;
   //拖动圆圈
-  function move(e, touch) {
-    e = e || window.event;
-    if (touch) {
-      e = touch;
+  function move(e,touch){
+    e=e||window.event;
+    if(touch){
+      e=touch;
     }
-    var L = Math.floor(e.clientX) - duration.offsetLeft;
-    if (L < 0) {
-      circles.style.left = 0 + 'px';
-      L = 0
-    } else if (L > duration.offsetWidth) {
-      circles.style.left = duration.offsetWidth + 'px';
-    } else {
-      circles.style.left = L + 'px';
+    var L=Math.floor(e.clientX)-duration.offsetLeft;
+    if(L<0){
+      circles.style.left=0+'px';
+      L=0
+    }else if(L>duration.offsetWidth){
+      circles.style.left=duration.offsetWidth+'px';
+    }else{
+      circles.style.left=L+'px';
     }
   }
-
-  function up() {
-    var L = circles.offsetLeft;
-    var when = L / duration.offsetWidth * bufferSource.buffer.duration;
-    acRebuild(when); //重新建立音频环境
-    document.body.removeEventListener('mousemove', move);
-    document.body.removeEventListener('mouseup', up);
+  function up(){
+    var L=circles.offsetLeft;
+    var when=L /duration.offsetWidth *bufferSource.buffer.duration;
+    acRebuild(when);//重新建立音频环境
+    document.body.removeEventListener('mousemove',move);
+    document.body.removeEventListener('mouseup',up);    
   }
-
-  function down(e, touch) {
+  function down(e,touch){
     window.cancelAnimationFrame(circlesAnimation);
-    document.body.addEventListener('mousemove', move, false);
-    document.body.addEventListener('mouseup', up, false);
-    circles.ontouchmove = function(e) {
-      touch = e.targetTouches[0];
-      move(e, touch)
+    document.body.addEventListener('mousemove',move,false);
+    document.body.addEventListener('mouseup',up,false);
+    circles.ontouchmove=function(e){
+      touch=e.targetTouches[0];
+      move(e,touch)
     }
-    circles.ontouchend = function(e) {
-      touch = e.targetTouches[0];
-      up(e, touch)
+    circles.ontouchend=function(e){
+      touch=e.targetTouches[0];
+      up(e,touch)
     }
   }
-  circles.onmousedown = function(e) {
-    e = e || window.event;
+  circles.onmousedown=function(e){
+    e=e||window.event;
     down(e);
     e.stopPropagation();
     e.preventDefault();
   };
-  circles.ontouchstart = function(e) {
-    var touch = e.targetTouches[0];
-    down(e, touch);
+  circles.ontouchstart=function(e){
+    var touch=e.targetTouches[0];
+    down(e,touch);
     e.stopPropagation();
     e.preventDefault();
   }
   //点击进度条
-  controlsTime.onmousedown = function(e) {
-    e = e || window.event;
+  controlsTime.onmousedown=function(e){
+    e=e||window.event;
     window.cancelAnimationFrame(circlesAnimation);
-    circles.style.left = e.clientX - duration.offsetLeft + 'px';
-    var when = (Math.floor(e.clientX) - duration.offsetLeft) /
-      duration.offsetWidth *
-      bufferSource.buffer.duration;
+    circles.style.left=e.clientX-duration.offsetLeft+'px';
+    var when=(Math.floor(e.clientX)-duration.offsetLeft)
+      /duration.offsetWidth
+      *bufferSource.buffer.duration;
     acRebuild(when);
     e.preventDefault();
   }
@@ -231,15 +229,15 @@ function sliding() {
 }
 
 //暂停与播放
-var resume = document.querySelector('.resume');
-var suspend = document.querySelector('.suspend');
-resume.onclick = function() {
-  ac.resume().then(function() {
+var resume=document.querySelector('.resume');
+var suspend=document.querySelector('.suspend');
+resume.onclick=function(){
+  ac.resume().then(function(){
     console.log('已恢复')
   })
 }
-suspend.onclick = function() {
-  ac.suspend().then(function() {
+suspend.onclick=function(){
+  ac.suspend().then(function(){
     console.log('暂停')
   });
 }
